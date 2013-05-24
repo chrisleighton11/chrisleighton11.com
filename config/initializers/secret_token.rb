69293cfef0cba05c04e2f3339731746992a9b63f
Chris Leighton <chrisleighton11@gmail.com>
2013-05-24 13:11:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chrisleighton11::Application.config.secret_token = '2f117cc54d34d636bceec446fc4c5333e2c3edfd079df5c007a3a83e2ca2db033347b5ef8aa5d4cd551fa749c2360185123e05ab51c7fe6c2e507b8f5f32508d'
